1e6e6099c5d1b9f3463255689c75710504a07690
Noel Phiri <59763043+Metro13@users.noreply.github.com>
2024-06-18 11:06:47 +0200

    test commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <div class="p-8 text-center">
     <h1 class="text-4xl font-bold text-purple-600">Create Todos</h1>
-    <p class="text-xl font-normal">Create and keep track of your Todos easily.</p>
+    <p class="text-xl font-normal">Create and keep track of your Todos easily with less effort.</p>
 </div>
 <Todos/>
 
